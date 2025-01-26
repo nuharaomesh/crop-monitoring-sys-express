04be5ff4063f4fd2e205e2e7587201ff92f4d4cf
nuharaomesh <omeshnuhara4@gmail.com>
2025-01-26 12:47:44 +0530

    set filupload as middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cropRoute from "./routes/crop-route";
+import fileUpload from 'express-fileupload'
 
 const app = express()
 app.use(express.json())
+app.use(fileUpload())
+
+app.use('/api/crop', cropRoute)
 
 export default app
