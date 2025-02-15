be822f14fa034e09bfe488b5baf2138269c36f2a
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-15 21:52:16 +0530

    modify header for CORS policy

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,23 @@ import fieldRoute from "./routes/field-route";
 import staffRoute from "./routes/staff-route";
 import logRoute from "./routes/log-route";
 import cultivateRoute from "./routes/cultivate-route";
+import { Request, Response } from "express";
 
 const app = express()
 app.use(express.json())
 app.use(fileUpload())
 
+app.use(function(req: Request, res: Response, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.use('/api/crop', cropRoute)
-app.use('/api/field', fieldRoute)
+app.use('/api/field', fieldRoute) 
 app.use('/api/staff', staffRoute)
-app.use('/api/log', logRoute)
+app.use('/api/log', logRoute) 
 app.use('/api/vehicle', vehicleRoute)
 app.use('/api/cultivation', cultivateRoute)
 
